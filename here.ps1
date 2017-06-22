db878e15b9d6b6f4fcb5a6f2bbb6a88f9b7e669a
PǝRV <alexander.w.matulionis@gmail.com>
2017-06-22 00:51:00 -0700

    Update here.ps1

--- here.ps1
+++ here.ps1
@@ -1,13 +1,32 @@
-Function Here($file) {
-  $PSDefaultParameterValues['*:Encoding'] = "utf8"
-  
-  if ($file -ne $null -and (test-path $file)) {
-    Get-Content $file | Set-Clipboard
+Function Here($emptyBodyOrFile, $emptyFileOrBody, $newFile) {
+  $PSDefaultParameterValues["*:Encoding"] = "utf8"
+
+  if ($emptyBodyOrFile -eq $null) {
+    Set-Clipboard $null
+    [System.Windows.Forms.SendKeys]::SendWait("here @'")
+  } elseif (Test-Path $emptyBodyOrFile) {
+    if ($emptyFileOrBody -eq $null) {
+      Get-Content $emptyBodyOrFile | Set-Clipboard
+      [System.Windows.Forms.SendKeys]::SendWait("here $emptyBodyOrFile @'")
+    } else {
+      if ($newFile -eq $null) {
+        Write-Output $emptyFileOrBody
+        return
+      } else {
+        "$emptyFileOrBody" | Out-FileUtf8NoBom $newFile
+        return
+      }
+    }
   } else {
-    Set-Clipboard $file
+    if ($emptyFileOrBody -eq $null) {
+      Write-Output $emptyBodyOrFile
+      return
+    } else {
+      "$emptyBodyOrFile" | Out-FileUtf8NoBom $emptyFileOrBody
+      return
+    }
   }
-  
-  [System.Windows.Forms.SendKeys]::SendWait("@'")
+
   [System.Windows.Forms.SendKeys]::SendWait("~")
   [System.Windows.Forms.SendKeys]::SendWait("^{v}")
 }
